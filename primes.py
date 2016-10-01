54278fd3d616e60528f1ab1403500e6cf4438a12
Gabriela Mogollón <gm5681@gmail.com>
2016-10-01 15:04:56 -0400

    Update primes.py (py1)

--- primes.py
+++ primes.py
@@ -1,16 +1,46 @@
 """
-Your module documentation here
+Primes module for vauxoo test.
+This module illustrates a Class to check prime numbers.
 """
+import math
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Prime Class to check if a integer is prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Return True if num_int is prime. Return False otherwise.
+
+        :param num_int: integer
+        :type num_int: int
+        :return: True if num_int is prime.
+        :rtype: bool
+
+        :Example:
+
+        >>> import primes
+        >>> p = primes.PrimeClass()
+        >>> p.is_prime(2)
+        True
+
+        .. todo:: check that num_int is int
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        # a prime is a natural number greater than 1
+        if num_int < 2:
+            return False
+        # 2 is prime
+        elif num_int == 2:
+            return True
+        # check for divisor
+        else:
+            top = math.floor(num_int/2)
+            while top >= 2:
+                if num_int % top == 0:
+                    return False
+                else:
+                    top = top-1
+
+            return True
